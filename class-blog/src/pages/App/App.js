48fb0224e7b2b58322348e3241ab17681e741968
timmmartinson <timmmartinson@gmail.com>
2023-03-09 13:51:32 -0700

    create auth page

--- class-blog/src/pages/App/App.js
+++ class-blog/src/pages/App/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 import AuthPage from '../AuthPage/AuthPage'
-import { getUser } from '../../utilities/users-services'
+import getUser from '../../utilities/users-services'
 
 function App() {
   const [user, setUser] = useState(getUser())
