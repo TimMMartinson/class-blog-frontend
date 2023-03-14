4bd231031f4ed3f6f512ff12c948cd8af8ecd7e5
timmmartinson <timmmartinson@gmail.com>
2023-03-14 07:19:45 -0600

    add comments

--- class-blog/src/pages/App/App.js
+++ class-blog/src/pages/App/App.js
@@ -5,6 +5,8 @@ import AuthPage from '../AuthPage/AuthPage'
 import NavBar from '../../components/NavBar'
 import HomePage from '../HomePage/HomePage';
 
+// Shoutout to Pat G. for this function, which lets the app know
+// whether to render the homepage or the AuthPage
 const getUser = () => {
   const user = localStorage.getItem('user')
   return user ? JSON.parse(user) : null;
